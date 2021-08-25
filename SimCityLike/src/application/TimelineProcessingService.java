5d132d7d6a71216b265189eebccd83e53f9a45d9
kashiwa54 <kashiwa54@gmail.com>
2021-08-25 12:54:45 +0900

    [CHANGE]Timeの変更に伴いメインプロセススレッドが持つ情報を変更

--- SimCityLike/src/application/TimelineProcessingService.java
+++ SimCityLike/src/application/TimelineProcessingService.java
@@ -4,10 +4,7 @@ import javafx.concurrent.ScheduledService;
 import javafx.concurrent.Task;
 
 public class TimelineProcessingService extends ScheduledService<Boolean>{
-	private int year = CommonConst.DEFAULT_YEAR;
-	private Season season = CommonConst.DEFAULUT_SEASON;
-	private Week week = CommonConst.DEFAULT_WEEK;
-	private Time time = new Time(0);
+	private Time time = new Time(CommonConst.DEFAULT_YEAR,CommonConst.DEFAULUT_SEASON,CommonConst.DEFAULT_WEEK,0,0);
 
 	@Override
 	protected Task<Boolean> createTask() {
@@ -16,33 +13,12 @@ public class TimelineProcessingService extends ScheduledService<Boolean>{
 			@Override
 			protected Boolean call() throws Exception {
 				time.add(1);
-				System.out.println(year + "年" + season.getJp() + week.getJp() + time.getHour() + ":" + time.getSecond());
+				System.out.println(time.getYear() + "年" + time.getSeason().getJp() + time.getWeek().getJp() + time.getHour() + ":" + time.getSecond());
 				return true;
 			}
 
 		};
 		return task;
-	}
-	public void setYear(int y)	{
-		this.year = y;
-	}
-	public void setSeason(Season s)		{
-		this.season = s;
-	}
-	public void setWeek(Week w)		{
-		this.week = w;
-	}
-	public void setTime(Time t)		{
-		this.time = t;
-	}
-	public int getYear()	{
-		return this.year;
-	}
-	public Season getSeason()	{
-		return this.season;
-	}
-	public Week getWeek()	{
-		return this.week;
 	}
 	public Time getTime()	{
 		return this.time;
