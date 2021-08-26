23ec0a6f57be46b88d8436058696dee3dc260dd6
kashiwa54 <kashiwa54@gmail.com>
2021-08-26 23:25:00 +0900

    [UPDATE]Consoleシステムを大幅改修

--- SimCityLike/src/application/ConsoleWindowController.java
+++ SimCityLike/src/application/ConsoleWindowController.java
@@ -35,87 +35,184 @@ public class ConsoleWindowController	{
 	@FXML
 	public void writeConsole(ActionEvent ae)	{					//イベントハンドラ(ActionEvent)で呼ばれるメソッド
 		String input;												//具体的にはコンソールに何か書いてEnterで呼ばれる
-		StringBuffer buffer = new StringBuffer();
+		StringBuilder builder = new StringBuilder();
 		int x = 0;
 		int y = 0;
 		int width = 0;
 		int height = 0;
-		buffer.append("Console Mode\n");
-		buffer.append("Please input command.\n");
+		builder.append("Console Mode\n");
+		builder.append("Please input command.\n");
 		input = textbox.getText();
 		textbox.setText("");
 		String[] arg = input.split(" ");
 		for(String value : arg)	{
-			buffer.append(value + "_");
+			builder.append(value + "_");
 		}
-		buffer.append("\n");
-		if(arg[0].equals("end"))	{
-			buffer.append("shutdown...\n");
-			//break;
-		}else if(arg[0].equals("write"))	{
-			for(int i = 0;i < this.map.getHeight();i++)	{
-				for(int j = 0;j < this.map.getWidth(); j++)	{
-					if(this.map.getTileObject(j,i) instanceof Space)	{
-						buffer.append("□");
-					}else if (this.map.getTileObject(j, i)instanceof Cottage){
-						buffer.append("△");
-					}else if (this.map.getTileObject(j, i)instanceof Residental){
-						buffer.append("住");
-					}
-				}
-				buffer.append("\n");
-			}
-		}else if(arg[0].equals("site"))	{
+		builder.append("\n");
+		
+		switch(arg[0])	{
+		case "end" : 
+			builder.append("shutdown...\n");
+			break;
+			
+		case "area" : 
 			try	{
 				x = Integer.parseInt(arg[2]);
 				y = Integer.parseInt(arg[3]);
 				width = Integer.parseInt(arg[4]);
 				height = Integer.parseInt(arg[5]);
-
+	
 			}catch(NumberFormatException e)	{
-				buffer.append("site command format is\n");
-				buffer.append("site [building name] [position x] [position y] [width] [height]\n");
+				builder.append("area command format is\n");
+				builder.append("area [building name] [position x] [position y] [width] [height]\n");
 			}
-			if(arg[1].equals("residental"))	{
+			switch(arg[1])	{
+			case "residental" : 
 				for(int i = 0;i <= width;i++)	{
 					for(int j = 0;j <= height;j++)	{
 						Residental r = new Residental(x + i,y + j);
 						this.map.place(r, r.getX(),r.getY());
-						buffer.append("Place residental in (" + r.getX() + "," + r.getY() + ") successful\n");
+						builder.append("Place residental in (" + r.getX() + "," + r.getY() + ") successful\n");
 					}
 				}
-
-			}else {
-				buffer.append("Cannot find this site name.\n");
+				break;
+			case "commercial" : 
+				for(int i = 0;i <= width;i++)	{
+					for(int j = 0;j <= height;j++)	{
+						Commercial r = new Commercial(x + i,y + j);
+						this.map.place(r, r.getX(),r.getY());
+						builder.append("Place commercial in (" + r.getX() + "," + r.getY() + ") successful\n");
+					}
+				}
+				break;
+			case "industrial" : 
+				for(int i = 0;i <= width;i++)	{
+					for(int j = 0;j <= height;j++)	{
+						Industrial r = new Industrial(x + i,y + j);
+						this.map.place(r, r.getX(),r.getY());
+						builder.append("Place industrial in (" + r.getX() + "," + r.getY() + ") successful\n");
+					}
+				}
+				break;
+			default : 
+				builder.append("Cannot find this area name.\n");
 			}
-		}else if(arg[0].equals("remove"))	{
+			break;
+			
+		case "remove" : 
 			try	{
 				x = Integer.parseInt(arg[1]);
 				y = Integer.parseInt(arg[2]);
-
+	
 			}catch(Exception e)	{
-				buffer.append("remove command format is\n");
-				buffer.append("remove [position x] [position y]\n");
+				builder.append("remove command format is\n");
+				builder.append("remove [position x] [position y]\n");
 			}
 			this.map.remove(x,y);
-		}else if(arg[0].equals("place"))	{
+			break;
+			
+		case "place" : 
 			try	{
+				boolean isFind = false;
 				x = Integer.parseInt(arg[2]);
 				y = Integer.parseInt(arg[3]);
-				if(arg[1].equals("cottage"))	{
-					Cottage c = new Cottage(x,y);
-					this.map.place(c, c.getX(),c.getY());
-					buffer.append("Place cottage in (" + c.getX() + "," + c.getY() + ") successful\n");
-				}else {
-					buffer.append("Cannot find this building name.\n");
+				for(ResidentalBuildingEnum b : ResidentalBuildingEnum.values()) {
+					if (b.getObjectClass().getSimpleName().equalsIgnoreCase(arg[1]))	{
+						TileObject o = b.getObject(x,y);
+						this.map.place(o, o.getX(),o.getY());
+						builder.append("Place in (" + o.getX() + "," + o.getY() + ") successful\n");
+						isFind = true;
+						break;
+					}
 				}
+				if (isFind)	break;
+				for(WayEnum b : WayEnum.values()) {
+					if (b.getObjectClass().getSimpleName().equalsIgnoreCase(arg[1]))	{
+						TileObject o = b.getObject(x,y);
+						this.map.place(o, o.getX(),o.getY());
+						builder.append("Place in (" + o.getX() + "," + o.getY() + ") successful\n");
+						isFind = true;
+						break;
+					}
+				}
+				if (isFind)	break;
+				builder.append("Cannot find this building name.\n");
 			}catch(Exception e)	{
-				buffer.append("place command format is\n");
-				buffer.append("place [position x] [position y]\n");
+				builder.append("place command format is\n");
+				builder.append("place [position x] [position y]\n");
 			}
-
+			break;
+			
+		default : 
+			builder.append("cannot find this command.\n");
 		}
-		textarea.appendText(buffer.toString());
+//		if(arg[0].equals("end"))	{
+//			builder.append("shutdown...\n");
+//			//break;
+//		}else if(arg[0].equals("write"))	{
+//			for(int i = 0;i < this.map.getHeight();i++)	{
+//				for(int j = 0;j < this.map.getWidth(); j++)	{
+//					if(this.map.getTileObject(j,i) instanceof Space)	{
+//						builder.append("□");
+//					}else if (this.map.getTileObject(j, i)instanceof Cottage){
+//						builder.append("△");
+//					}else if (this.map.getTileObject(j, i)instanceof Residental){
+//						builder.append("住");
+//					}
+//				}
+//				builder.append("\n");
+//			}
+//		}else if(arg[0].equals("site"))	{
+//			try	{
+//				x = Integer.parseInt(arg[2]);
+//				y = Integer.parseInt(arg[3]);
+//				width = Integer.parseInt(arg[4]);
+//				height = Integer.parseInt(arg[5]);
+//
+//			}catch(NumberFormatException e)	{
+//				builder.append("site command format is\n");
+//				builder.append("site [building name] [position x] [position y] [width] [height]\n");
+//			}
+//			if(arg[1].equals("residental"))	{
+//				for(int i = 0;i <= width;i++)	{
+//					for(int j = 0;j <= height;j++)	{
+//						Residental r = new Residental(x + i,y + j);
+//						this.map.place(r, r.getX(),r.getY());
+//						builder.append("Place residental in (" + r.getX() + "," + r.getY() + ") successful\n");
+//					}
+//				}
+//
+//			}else {
+//				builder.append("Cannot find this site name.\n");
+//			}
+//		}else if(arg[0].equals("remove"))	{
+//			try	{
+//				x = Integer.parseInt(arg[1]);
+//				y = Integer.parseInt(arg[2]);
+//
+//			}catch(Exception e)	{
+//				builder.append("remove command format is\n");
+//				builder.append("remove [position x] [position y]\n");
+//			}
+//			this.map.remove(x,y);
+//		}else if(arg[0].equals("place"))	{
+//			try	{
+//				x = Integer.parseInt(arg[2]);
+//				y = Integer.parseInt(arg[3]);
+//				if(arg[1].equals("cottage"))	{
+//					Cottage c = new Cottage(x,y);
+//					this.map.place(c, c.getX(),c.getY());
+//					builder.append("Place cottage in (" + c.getX() + "," + c.getY() + ") successful\n");
+//				}else {
+//					builder.append("Cannot find this building name.\n");
+//				}
+//			}catch(Exception e)	{
+//				builder.append("place command format is\n");
+//				builder.append("place [position x] [position y]\n");
+//			}
+//
+//		}
+		textarea.appendText(builder.toString());
 		mwc.paintMainCanvas(graphics,map);
 	}
 	public void write(String str)	{
