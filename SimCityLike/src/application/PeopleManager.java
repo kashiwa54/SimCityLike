685288e50464fb78f52ac6842668a4757f0eabde
kashiwa54 <kashiwa54@gmail.com>
2021-09-01 16:34:21 +0900

    [ADD]PeopleManagerにメソッドを追加

--- SimCityLike/src/application/PeopleManager.java
+++ SimCityLike/src/application/PeopleManager.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class PeopleManager {
 	public static final int INISIAL_CAPACITY = CommonConst.PEOPLE_INISIAL_CAPACITY;
@@ -10,7 +11,7 @@ public class PeopleManager {
 	private Map fieldMap = null;
 
 	private ArrayList<People> homelessList = new ArrayList<People>(INISIAL_CAPACITY);
-	private ArrayList<ResidentalBuilding> residentalList = new ArrayList<ResidentalBuilding>(INISIAL_CAPACITY);
+	private List<ResidentalBuilding> residentalList = ResidentalBuilding.residentalList;;
 
 	public PeopleManager(Time worldTime)	{
 		this.worldClock = worldTime;
@@ -28,6 +29,20 @@ public class PeopleManager {
 				return false;
 			}
 		}
+	}
+
+	public People createPeople(int age)	{
+		Time birthday = worldClock.clone();
+		birthday.backYear(age);
+		People p = new People(birthday,worldClock);
+		homelessList.add(p);
+		return p;
+	}
+	public People birthPeople()	{
+		Time birthday = worldClock.clone();
+		People p = new People(birthday,worldClock);
+		homelessList.add(p);
+		return p;
 	}
 
 }
