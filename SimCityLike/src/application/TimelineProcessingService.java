a16e4cfe074a507bd3329efa9762932ee150a1e8
kashiwa54 <kashiwa54@gmail.com>
2021-09-06 11:10:14 +0900

    [ADD]時間間隔ごとの処理を行う準備を追加

--- SimCityLike/src/application/TimelineProcessingService.java
+++ SimCityLike/src/application/TimelineProcessingService.java
@@ -5,6 +5,12 @@ import javafx.concurrent.Task;
 
 public class TimelineProcessingService extends ScheduledService<Boolean>{
 	private Time time;
+	private Time previousTime;
+	private Boolean switchHour = false;
+	private Boolean switchNoon = false;
+	private Boolean switchDay = false;
+	private Boolean switchSeason = false;
+	private Boolean switchYear = false;
 	public TimelineProcessingService(Time worldTime)	{
 		this.time = worldTime;
 	}
@@ -15,7 +21,34 @@ public class TimelineProcessingService extends ScheduledService<Boolean>{
 
 			@Override
 			protected Boolean call() throws Exception {
+				previousTime = time.clone();
 				time.addSecond(CommonConst.DEFAULT_INCREMENT_SECOND);
+
+				if(previousTime.getHour() != time.getHour())	{
+					switchHour = true;
+					if(time.getHour() % 12 == 0)	{
+						switchNoon = true;
+					}else {
+					}
+					if(previousTime.getWeek() != time.getWeek())	{
+						switchDay = true;
+						if(previousTime.getSeason() != time.getSeason())	{
+							switchSeason = true;
+							if(previousTime.getYear() != time.getYear())	{
+								switchYear = true;
+							}
+						}
+					}
+				}else {
+					switchHour = false;
+					switchNoon = false;
+					switchDay = false;
+					switchSeason = false;
+					switchYear = false;
+				}
+				if(switchNoon)	{
+
+				}
 				return true;
 			}
 
