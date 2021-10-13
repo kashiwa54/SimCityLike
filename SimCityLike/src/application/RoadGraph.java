01efab4a8072bdd3c36194da2a7791dd8e69b015
kashiwa54 <kashiwa54@gmail.com>
2021-10-13 16:40:34 +0900

    [FIX]ループするとフリーズするバグを修正
    [ADD]最短距離ルート決定アルゴリズムを途中まで追加

--- SimCityLike/src/application/RoadGraph.java
+++ SimCityLike/src/application/RoadGraph.java
@@ -2,16 +2,103 @@ package application;
 
 import java.util.EnumSet;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Stack;
 
 public class RoadGraph {
 	private GraphNode startNode;
-	private LinkedList<Road> nodeRoadList = new LinkedList<Road>();
+	private LinkedList<GraphNode> nodeList = new LinkedList<GraphNode>();
 
 	public RoadGraph(Road road)	{
 		startNode = new GraphNode(road);
-
+		nodeList.add(startNode);
 		createGraph(startNode);
+	}
+
+	public LinkedList<GraphNode> calcPath(Road road)	{
+		PriorityQueue<GraphNode> queue = new PriorityQueue<GraphNode>();
+		GraphNode doneNode;
+		boolean dupflg = false;
+		for(GraphNode n : nodeList)	{
+			if(n.getRoad() == road) {
+				dupflg = true;
+				queue.add(n);
+				break;
+			}
+		}
+		if(!dupflg)	{
+			GraphNode newNode = new GraphNode(road);
+			addNode(newNode);
+			queue.add(newNode);
+		}
+		while(!queue.isEmpty())	{
+			doneNode = queue.poll();
+			doneNode.setDone(true);
+			GraphNode[] edgeTo = doneNode.getEdgeTo();
+			int[] edgeCost = doneNode.getEdgeCost();
+			for(int i = 0; i < 4; i++)	{
+				if((edgeTo[i] != null)&&(!edgeTo[i].getDone()))	{
+					int cost = edgeCost[i] + doneNode.getCost();
+					if((cost < edgeTo[i].getCost())||(edgeTo[i].getCost() < 0))	{
+						edgeTo[i].setCost(cost);
+						edgeTo[i].setFrom(doneNode);
+						if(!queue.contains(edgeTo[i]))	{
+							queue.add(edgeTo[i]);
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	public GraphNode[] addNode(GraphNode node)	{
+		GraphNode[] newNodes = new GraphNode[3];
+		int index = 0;
+		boolean dupflg = false;
+		if(node.getRoad().getConnect().size() >= 2)	{
+			for(Direction d : node.getRoad().getConnect())	{
+				int dCost = 1;
+				Way nextWay = node.getRoad();
+				Direction nextDir = d;
+				EnumSet<Direction> connection = nextWay.getConnectWay(nextDir).getConnect();
+				while(connection.size() == 2)	{
+					boolean loopflg = false;
+					dCost++;
+					for(GraphNode n : nodeList)	{
+						if (n.getRoad() == (Road)nextWay) loopflg = true;
+					}
+					if(loopflg) break;
+					nextWay = nextWay.getConnectWay(nextDir);
+					for(Direction dir : connection)	{
+						if(dir != Direction.reverse(d))	{
+							nextDir = dir;
+						}
+					}
+					connection = nextWay.getConnectWay(nextDir).getConnect();
+				}
+				for(GraphNode n : nodeList)	{
+					if(n.getRoad() == (Road)nextWay)	{
+						dupflg = true;
+						node.addEdge(n,d,dCost);
+						break;
+					}
+				}
+				if(!dupflg)	{
+					GraphNode newNode = new GraphNode((Road) nextWay);
+					node.addEdge(newNode, d, dCost);
+					nodeList.add(newNode);
+					newNodes[index] = newNode;
+					index++;
+				}
+
+			}
+		}
+		return newNodes;
+	}
+	public GraphNode[] addNode(Road road)	{
+		GraphNode node = new GraphNode(road);
+		return addNode(node);
 	}
 
 	private void createGraph(GraphNode startNode)	{
@@ -19,26 +106,10 @@ public class RoadGraph {
 		stack.push(startNode);
 		while(!stack.empty())	{
 			GraphNode node = stack.pop();
-			if(node.getRoad().getConnect().size() >= 2)	{
-				for(Direction d : node.getRoad().getConnect())	{
-					int dCost = 1;
-					Way nextWay = node.getRoad();
-					Direction nextDir = d;
-					EnumSet<Direction> connection = nextWay.getConnectWay(nextDir).getConnect();
-					while(connection.size() == 2)	{
-						dCost++;
-						nextWay = nextWay.getConnectWay(nextDir);
-						for(Direction dir : connection)	{
-							if(dir != Direction.reverse(d))	{
-								nextDir = dir;
-							}
-						}
-						connection = nextWay.getConnectWay(nextDir).getConnect();
-					}
-					if(!nodeRoadList.contains((Road)nextWay))	{
-						node.addNode(new GraphNode((Road) nextWay), d, dCost);
-						nodeRoadList.add((Road)nextWay);
-					}
+			GraphNode[] newNodes = addNode(node);
+			for(GraphNode n : newNodes)	{
+				if(n != null)	{
+					stack.push(n);
 				}
 			}
 		}
@@ -47,7 +118,7 @@ public class RoadGraph {
 
 }
 
-class GraphNode {
+class GraphNode implements Comparable<GraphNode>{
 	private Road road;
 
 	private GraphNode[] edgeTo = new GraphNode[4];
@@ -66,6 +137,12 @@ class GraphNode {
 	public Road getRoad()	{
 		return road;
 	}
+	public GraphNode[] getEdgeTo()	{
+		return edgeTo;
+	}
+	public int[] getEdgeCost()	{
+		return edgeCost;
+	}
 	public GraphNode getFrom()	{
 		return from;
 	}
@@ -75,7 +152,16 @@ class GraphNode {
 	public int getCost()	{
 		return cost;
 	}
-	public void addNode(GraphNode node,Direction d,int cost)	{
+	public void setDone(boolean done)	{
+		this.done = done;
+	}
+	public void setCost(int cost)	{
+		this.cost = cost;
+	}
+	public void setFrom(GraphNode node)	{
+		this.from = node;
+	}
+	public void addEdge(GraphNode node,Direction d,int cost)	{
 		int index = convertDirection(d);
 		edgeTo[index] = node;
 		edgeCost[index] = cost;
@@ -84,6 +170,7 @@ class GraphNode {
 		node.edgeTo[rIndex] = this;
 		node.edgeCost[rIndex] = cost;
 	}
+
 
 	private int convertDirection(Direction d)	{
 		int index = 0;
@@ -105,5 +192,16 @@ class GraphNode {
 		}
 
 		return index;
+	}
+
+	@Override
+	public int compareTo(GraphNode o) {
+		if(this.cost == o.cost)	{
+			return 0;
+		}else if(this.cost < o.cost) {
+			return -1;
+		}else {
+			return 1;
+		}
 	}
 }
